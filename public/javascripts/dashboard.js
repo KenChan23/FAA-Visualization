8d4114fcc0c32e594cb5f99c1e88158b79706599
Ken Chan <KChan923@gmail.com>
2015-04-27 18:07:58 -0400

    modified numerical scale for color scale

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -1,30 +1,42 @@
 (function(){
 
-  var MAX_FATALITIES = 13;
-  var MAX_ACCIDENTS = 13;
+  //  Initialize scale 
+  var MAX_FATALITIES = 37;
+  var MAX_ACCIDENTS = 44;
 
   var data_lookup = {}; 
   var accident_data;
   var fatality_data;
+  var barchartA_data;
 
   var width = 750,
       height = 100,
       // cellSize = 17; // cell size
       cellSize = 13;
 
+  var chartWidth = 300,
+      chartHeight = 250;
+
   var day = d3.time.format("%w"),
       week = d3.time.format("%U"),
       format = d3.time.format("%Y-%m-%d");
 
-  var fatality_color = d3.scale.quantize()
-      .domain([0, MAX_FATALITIES])
-      .range(d3.range(MAX_FATALITIES).map(function(d) { return "q-fatality" + d + "-13"; }));
+  var fatality_color = d3.scale.quantile()
+      .domain([1, MAX_FATALITIES])
+      .range(d3.range(1, 9).map(function(d){
+        return "q-color" + d + "-10";
+      }));
+
+  console.log(fatality_color(30));
       //  Map input domain to discrete range.
 
-  var accident_color = d3.scale.quantize()
-      .domain([0, MAX_ACCIDENTS])
-      .range(d3.range(MAX_ACCIDENTS).map(function(d) { return "q-fatality" + d + "-13"; }));
+  var accident_color = d3.scale.quantile()
+      .domain([1, MAX_ACCIDENTS])
+      .range(d3.range(1, 9).map(function(d){
+        return "q-color" + d + "-10";
+      }));
 
+  //  append heatmap
   var svg = d3.select("#heatmap-panel").selectAll("svg")
       .data(d3.range(1980, 1985))
     .enter().append("svg")
@@ -58,6 +70,37 @@
       .attr("class", "month")
       .attr("d", monthPath);
 
+  //  append bar charts
+  // var svgA = d3.select("body").select("#bar-charts").select("#bar-chart-a").append("svg")
+  //   .attr("width", chartWidth)
+  //   .attr("height", chartHeight)
+  //   .style('background-color','black')
+  //   .append("g")
+  //   .attr("transform", "translate(" + 0 + "," + 0 + ")")
+  //   .append("text").attr("class", "title").attr("x", ).attr("y", -26)
+  //     .text("Total Hours Flown - Past 90 Days");
+
+  // var svgB = d3.select("body").select("#bar-charts").select("#bar-chart-b").append("svg")
+  //   .attr("width", chartWidth)
+  //   .attr("height", chartHeight)
+  //   .style('background-color','red')
+  // .append("g")
+  //   .attr("transform", "translate(" + 0 + "," + 0 + ")");
+
+  // var svgC = d3.select("body").select("#bar-charts").select("#bar-chart-c").append("svg")
+  //   .attr("width", chartWidth)
+  //   .attr("height", chartHeight)
+  //   .style('background-color','blue')
+  // .append("g")
+  //   .attr("transform", "translate(" + 0 + "," + 0 + ")");
+
+  // var svgD = d3.select("body").select("#bar-charts").select("#bar-chart-d").append("svg")
+  //   .attr("width", chartWidth)
+  //   .attr("height", chartHeight)
+  //   .style('background-color','green')
+  // .append("g")
+  //   .attr("transform", "translate(" + 0 + "," + 0 + ")");
+
   d3.csv("../data/part_91_csv/data_1980_84.csv", function(error, csv) {
 
     csv.forEach(function(d){
@@ -71,19 +114,44 @@
       .key(function(d) { return d.date; })
       .rollup(function(d) { 
         //  Count of fatalities...
-         return d3.sum(d, function(g){ return (g.fatalities !== null) ? parseInt(g.fatalities) : 0; });
+         fatality_sum = d3.sum(d, function(g){
+           if (g.fatalities !== null)
+          {
+              fatalities = parseInt(g.fatalities);
+            
+            return fatalities;
+          }
+          else 
+              return 0; 
+          });
+
+         // if(MAX_FATALITIES < fatality_sum)
+         //      MAX_FATALITIES = fatality_sum;
+         //    console.log("Fatalities: " + MAX_FATALITIES);
+         // console.log("Sum: " + fatality_sum);
+         return fatality_sum;
       }).map(csv);
 
     accident_data = d3.nest()
       .key(function(d){ return d.date; })
       .rollup(function(d){
         //  Count of accidents...
+        // if(d.length > MAX_ACCIDENTS)
+        //   MAX_ACCIDENTS = d.length;
+        // console.log("Accidents: " + MAX_ACCIDENTS);
         return d.length; 
       }).map(csv);
 
+    // barchartA_data = d3.nest()
+    //   .key(function(d){ return d.date; })
+    //   .rollup(function(d){
+    //     //  Count of accidents...
+    //     return d.length; 
+    //   }).map(csv);
+
     //  Initialize the view.
     rect.filter(function(d) { return d in fatality_data; })
-      .attr("class", function(d) { return "day " + fatality_color(fatality_data[d]); })
+      .attr("class", function(d) { return (fatality_data[d] === 0) ? "day q-color" + d + "-10" : "day " + fatality_color(fatality_data[d]); })
       .select("title")
         .text(function(d) {
           if(fatality_data[d] == 0)
@@ -133,7 +201,7 @@
     function renderRectangle(data, color, id){
       console.log("inside default");
         rect.filter(function(d) { return d in data; })
-          .attr("class", function(d) { return "day " + color(data[d]); })
+          .attr("class", function(d) { return (data[d] === 0) ? "day q-color" + d + "-10" : "day " + color(data[d]); })
           .select("title")
             .text(function(d) {
               switch(id)
@@ -199,4 +267,90 @@
         return d3.select(this).classed('q-invisible', true);
     });
   });
+
+  // function generateBarChartA(data) {
+  //   var x = d3.scale.ordinal()
+  //   .rangeRoundBands([0, width], .1, .3);
+
+  //   var y = d3.scale.linear()
+  //       .range([height, 0]);
+
+  //   var xAxis = d3.svg.axis()
+  //       .scale(x)
+  //       .orient("bottom");
+
+  //   var yAxis = d3.svg.axis()
+  //       .scale(y)
+  //       .orient("left")
+  //       .ticks(8, "%");
+
+  // var svg = d3.select("body").select("#bar-chart-a").append("svg")
+  //   .attr("width", width + margin.left + margin.right)
+  //   .attr("height", height + margin.top + margin.bottom)
+  //   .append("g")
+  //   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  //   d3.tsv("data.tsv", type, function(error, data) {
+  // x.domain(data.map(function(d) { return d.name; }));
+  // y.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+  // svg.append("text")
+  //     .attr("class", "title")
+  //     .attr("x", x(data[0].name))
+  //     .attr("y", -26)
+  //     .text("Total Hours Flown - Past 90 Days");
+
+  // svg.append("g")
+  //     .attr("class", "x axis")
+  //     .attr("transform", "translate(0," + height + ")")
+  //     .call(xAxis)
+  //   .selectAll(".tick text")
+  //     .call(wrap, x.rangeBand());
+
+  // svg.append("g")
+  //     .attr("class", "y axis")
+  //     .call(yAxis);
+
+  // svg.selectAll(".bar")
+  //     .data(data)
+  //   .enter().append("rect")
+  //     .attr("class", "bar")
+  //     .attr("x", function(d) { return x(d.name); })
+  //     .attr("width", x.rangeBand())
+  //     .attr("y", function(d) { return y(d.value); })
+  //     .attr("height", function(d) { return height - y(d.value); });
+  // });
+
+  // }
+
+  
+
+    function type(d) {
+    d.value = +d.value;
+    return d;
+  }
+
+  function wrap(text, width) {
+  text.each(function() {
+    var text = d3.select(this),
+        words = text.text().split(/\s+/).reverse(),
+        word,
+        line = [],
+        lineNumber = 0,
+        lineHeight = 1.1, // ems
+        y = text.attr("y"),
+        dy = parseFloat(text.attr("dy")),
+        tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+    while (word = words.pop()) {
+      line.push(word);
+      tspan.text(line.join(" "));
+      if (tspan.node().getComputedTextLength() > width) {
+        line.pop();
+        tspan.text(line.join(" "));
+        line = [word];
+        tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+      }
+    }
+  });
+}
 })();
