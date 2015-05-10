e9c01eacfcaa5885041ac8f52f38747f28e59dfe
Ken Chan <KChan923@gmail.com>
2015-05-10 02:55:09 -0400

    added full remarks

--- public/javascripts/collapsible.js
+++ public/javascripts/collapsible.js
@@ -4,8 +4,9 @@ var collapsibleTreeModule = (function(){
   var collapsibleTree = {};
 
   collapsibleTree.create = function(data){
-    console.log(data);
+    // console.log(data);
     var date_range;
+    var remarks_hash = {};
 
     if(data[data.length - 1].date != data[0].date)
       date_range = data[0].date + " to " + data[data.length - 1].date; 
@@ -20,32 +21,105 @@ var collapsibleTreeModule = (function(){
 
         for(var property in d.values[i])
          {
+          var truncated_text;
+          var property_modified;
           if(property !== "name")
           {
+            if(d.values[i][property] == null || d.values[i][property] == "null")
+              d.values[i][property] = "Unknown";
+
             switch(property)
             {
               case 'date':
-                property = 'Date'
+                property_modified = 'Date';
+                flare_obj.children[index].children[i].children.push({name: property_modified + ": " + d.values[i][property], class: "child"});
                 break;
               case 'state':
-                property = 'State'
+                property_modified = 'State';
+                flare_obj.children[index].children[i].children.push({name: property_modified + ": " + d.values[i][property], class: "child"});
                 break;
               case 'city':
-                property = 'City';
+                property_modified = 'City';
+                flare_obj.children[index].children[i].children.push({name: property_modified + ": " + d.values[i][property], class: "child"});
+                break;
+              case 'weather':
+                property_modified = 'Weather';
+                flare_obj.children[index].children[i].children.push({name: property_modified + ": " + d.values[i][property], class: "child"});
                 break;
               case 'light':
-                property = 'Light Condition';
+                property_modified = 'Light Condition';
+                flare_obj.children[index].children[i].children.push({name: property_modified + ": " + d.values[i][property], class: "child"});
                 break;
-              case 'city':
-                property = 'City';
+              case 'visibility':
+                property_modified = 'Visibility (in Miles)';
+                flare_obj.children[index].children[i].children.push({name: property_modified + ": " + d.values[i][property], class: "child"});
                 break;
-              case 'city':
-                property = 'City';
+              case 'fatalities':
+                property_modified = 'Fatalities';
+                flare_obj.children[index].children[i].children.push({name: property_modified + ": " + d.values[i][property], class: "child"});
+                break;
+              case 'primary_cause':
+                property_modified = 'Primary Cause';
+                flare_obj.children[index].children[i].children.push({name: property_modified + ": " + d.values[i][property], class: "child"});
+                break;
+              case 'type':
+                property_modified = 'Type of Accident';
+                flare_obj.children[index].children[i].children.push({name: property_modified + ": " + d.values[i][property], class: "child"});
+                break;
+              case 'pilot_age':
+                property_modified = 'Pilot Age (in Years)';
+                flare_obj.children[index].children[i].children.push({name: property_modified + ": " + d.values[i][property], class: "child"});
+                break;
+              case 'total_hours_model_flown':
+                property_modified = 'Total Number of Hours Flown Given Model';
+                flare_obj.children[index].children[i].children.push({name: property_modified + ": " + d.values[i][property], class: "child"});
+                break;
+              case 'hours_model_flown_90_days':
+                property_modified = 'Total Number of Hours Flown Given Model (Past 90 Days)';
+                flare_obj.children[index].children[i].children.push({name: property_modified + ": " + d.values[i][property], class: "child"});
+                break;
+              case 'hours_flown_90_days':
+                property_modified = 'Total Number of Hours Flown (Past 90 Days)';
+                flare_obj.children[index].children[i].children.push({name: property_modified + ": " + d.values[i][property], class: "child"});
+                break;
+              case 'total_hours_flown':
+                property_modified = 'Total Number of Hours Flown';
+                flare_obj.children[index].children[i].children.push({name: property_modified + ": " + d.values[i][property], class: "child"});
+                break;
+              case 'pilot_certification':
+                property_modified = 'Pilot Certification';
+                flare_obj.children[index].children[i].children.push({name: property_modified + ": " + d.values[i][property], class: "child"});
+                break;
+              case 'engines':
+                property_modified = 'Total Number of Engines';
+                flare_obj.children[index].children[i].children.push({name: property_modified + ": " + d.values[i][property], class: "child"});
+                break;
+              case 'aircraft_make':
+                property_modified = 'Aircraft Make';
+                flare_obj.children[index].children[i].children.push({name: property_modified + ": " + d.values[i][property], class: "child"});
+                break;
+              case 'aircraft_model':
+                property_modified = 'Aircraft Model';
+                flare_obj.children[index].children[i].children.push({name: property_modified + ": " + d.values[i][property], class: "child"});
+                break;
+              case 'hours_airframe':
+                property_modified = 'Total Airframe Hours';
+                flare_obj.children[index].children[i].children.push({name: property_modified + ": " + d.values[i][property], class: "child"});
+                break;
+              case 'remarks':
+                property_modified = 'Remarks';
+                truncated_text = d.values[i][property].substring(0, 48) + "...";
+                remarks_hash[truncated_text] = {date: d.values[i].date, primary_cause: d.key, event_number: i, text: d.values[i][property]};
+                (d.values[i][property].length > 47) ? d.values[i]['modified_remarks'] = truncated_text : d.values[i]['modified_remarks'] = d.values[i][property];
+                flare_obj.children[index].children[i].children.push({name: property_modified + ": " + d.values[i]['modified_remarks'], class: "child"});
+                break;
+              default: 
                 break;
             }
-
             // console.log(property);
-            flare_obj.children[index].children[i].children.push({name: property + ": " + d.values[i][property], class: "child"});
+            
+            // console.log(property);
+
           }
          } 
          } });
@@ -96,7 +170,8 @@ var collapsibleTreeModule = (function(){
       // });
 
       function update(source) {
-
+        console.log("Source");
+        console.log(root);
         // Compute the flattened node list. TODO use d3.layout.hierarchy.
         var nodes = treeFlare.nodes(root);
 
@@ -126,13 +201,15 @@ var collapsibleTreeModule = (function(){
             .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
             .style("opacity", 1e-6);
 
+
         // Enter any new nodes at the parent's previous position.
         nodeEnter.append("rect")
             .attr("y", -barHeightFlare / 2)
             .attr("height", barHeightFlare)
             .attr("width", barWidthFlare)
             .style("fill", color)
-            .on("click", click);
+            .on("click", click)
+            .on("mouseover", hoverRect);
 
         nodeEnter.append("text")
             .attr("dy", 3.5)
@@ -211,6 +288,18 @@ var collapsibleTreeModule = (function(){
       function color(d) {
         return d._children ? "#3182bd" : d.children ? "#c6dbef" : "#fd8d3c";
       }
+
+      console.log(remarks_hash);
+
+      function hoverRect(d) {
+        if(d.name.indexOf("Remarks") >= 0)
+          // d3.select('#remarks-box').html("<div>Date: " + remarks_hash[d.name.substring(9)].date 
+          //   + "</div><div>Primary Cause: " + remarks_hash[d.name.substring(9)].primary_cause 
+          //   +  "</div><div>Event Number: " + remarks_hash[d.name.substring(9)].event_number 
+          //   +  "</div><div>Remarks: " + remarks_hash[d.name.substring(9)].text + "</div>");
+          d3.select('#remarks-box').html("<h5>Full Remarks: </h5>" + "<div>" + remarks_hash[d.name.substring(9)].text + "</div>");
+      }
+
     };
 
     collapsibleTree.update = function(){
